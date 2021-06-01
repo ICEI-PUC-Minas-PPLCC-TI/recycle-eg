592a004ac1be9564ab15c1888f4d9736d3c01114
Pedro Pereira Lourenço <pedro.lourenco.1171956@sga.pucminas.br>
2021-06-01 17:06:20 -0300

    pastas dentro de recycle-eg-mates extraidas

---
+++ src/scripts/caixaLogin.js
@@ -0,0 +1,11 @@
+
+// Animação do login
+window.onload = () =>{
+
+    document.querySelector(".popup").classList.add("active");
+    cadastroUsuario = document.querySelector("#registrar");
+    cadastroUsuario.style.display = "none";
+  };
+
+
+

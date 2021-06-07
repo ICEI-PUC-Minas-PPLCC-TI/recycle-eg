d349b8fc5209d58550b3529a69b9bc8b76fcd2ad
mimii2003 <mbarreira@sga.pucminas.br>
2021-06-07 11:01:14 -0300

    adicionar imagem
    mudei o html e o javaScrip

--- src/scripts/user_info.js
+++ src/scripts/user_info.js
@@ -269,3 +269,24 @@ function setUserPass() {
 initLoginApp();
 
 $(".nome").val(usuarioCorrente.nome);
+function readURL(input) {
+    if (input.files && input.files[0]) {
+  
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+        $('.image-upload-wrap').hide();
+  
+        $('.file-upload-image').attr('src', e.target.result);
+        $('.file-upload-content').show();
+  
+        $('.image-title').html(input.files[0].name);
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+  
+    } else {
+      removeUpload();
+    }
+  }
+  
